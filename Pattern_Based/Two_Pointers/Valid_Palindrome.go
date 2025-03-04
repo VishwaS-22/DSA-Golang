b6b6a49f0ddb5dd49c84bcf12c37a9dcfc3aa2a3
VishwaS-22 <vishwa20042003@gmail.com>
2025-03-05 00:16:59 +0530

    Added 2nd Prob in 2Pointers, E

---
+++ Pattern_Based/Two_Pointers/Valid_Palindrome.go
@@ -0,0 +1,32 @@
+package main
+
+func isAlphaNumeric(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch >= 'b') || (ch >= '0' && ch <= '9')
+}
+
+func toLower(ch byte) byte {
+	if ch >= 'A' && ch <= 'Z' {
+		return ch - 'A' + 'a'
+	}
+	return ch
+}
+
+func isPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+
+	for l < r {
+		for l < r && !isAlphaNumeric(s[l]) {
+			l++
+		}
+		for l < r && !isAlphaNumeric(s[r]) {
+			r--
+		}
+
+		if toLower(s[l]) != toLower(s[r]) {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
--- Pattern_Based/Two_Pointers/main.go
+++ Pattern_Based/Two_Pointers/main.go
@@ -3,17 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter size of an arr: ")
-	var s int
+	// fmt.Print("Enter size of an arr: ")
+	// var s int
+	// fmt.Scan(&s)
+	// fmt.Print("Enter vals of arr: ")
+	// arr := make([]int, s)
+	// for i := range arr {
+	// 	fmt.Scan(&arr[i])
+	// }
+	// fmt.Print("Enter target val to look for two sum: ")
+	// var t int
+	// fmt.Scan(&t)
+	// ans := two_sum(arr, t)
+	// fmt.Printf("The 1 based index is: %d", ans)
+	fmt.Print("Enter a input string: ")
+	var s string
 	fmt.Scan(&s)
-	fmt.Print("Enter vals of arr: ")
-	arr := make([]int, s)
-	for i := range arr {
-		fmt.Scan(&arr[i])
+	if isPalindrome(s) {
+		fmt.Println("Yes, It's a Palindrome.")
+	} else {
+		fmt.Println("No, it's not a Palindrome.")
 	}
-	fmt.Print("Enter target val to look for two sum: ")
-	var t int
-	fmt.Scan(&t)
-	ans := two_sum(arr, t)
-	fmt.Printf("The 1 based index is: %d", ans)
 }
